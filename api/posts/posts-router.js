431edf345db78acdaa6645cc06b2a03bcb377807
SylvaDev <irvingcsylva@gmail.com>
2025-06-10 19:18:47 -0700

    GET post by id fleshed out and working with error handling

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -8,13 +8,28 @@ router.use(express.json());
 // Posts Endpoints
 // Posts Endpoints
 router.get('/', (req, res) => {
-    Posts.find(req.query)
+    Posts.find(req.body)
     .then(posts => {
         res.status(200).json(posts);
     })
     .catch(err => {
         console.log(err);
         res.status(500).json({ message: 'The posts information could not be retrieved' });
+    })
+})
+
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+    .then(post => {
+        if (post){
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({ message: 'The post with the specified ID does not exist' });
+        }        
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: 'The post information could not be retrieved' });
     })
 })
 
