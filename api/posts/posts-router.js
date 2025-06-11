08bbc541ca1edeb8cf655549e2e879d770cf76c9
SylvaDev <irvingcsylva@gmail.com>
2025-06-10 18:55:29 -0700

    added express package and set up express router and api

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,12 @@
-// implement your posts router here
+const express = require('express');
+const Posts = require('./posts-model');
+
+const router = express.Router();
+
+router.use(express.json());
+
+// Posts Endpoints
+// Posts Endpoints
+
+
+module.exports = router;
